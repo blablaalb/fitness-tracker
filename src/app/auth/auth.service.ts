ed7bf58b00ccf21ddee98ec02d13ac9d9801aeef
blablaalb <55081715+blablaalb@users.noreply.github.com>
2022-02-10 23:06:05 +0400

    init commit

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,51 @@
+import {Subject} from "rxjs";
+
+import {User} from './user.model';
+import {AuthData} from "./auth-data.model";
+import {Injectable} from "@angular/core";
+import {Router} from "@angular/router";
+
+
+@Injectable()
+export class AuthService {
+  authChange = new Subject<boolean>();
+  private user!: User;
+
+  constructor(private router: Router) {
+  }
+
+  registerUser(authData: AuthData): void {
+    this.user = {
+      email: authData.email,
+      userId: Math.round(Math.random() * 10000).toString()
+    };
+    this.authSuccessfully();
+  }
+
+  login(authData: AuthData): void {
+    this.user = {
+      email: authData.email,
+      userId: Math.round(Math.random() * 10000).toString()
+    };
+    this.authSuccessfully();
+  }
+
+  logout(): void {
+    this.user = null;
+    this.authChange.next(true);
+    this.router.navigate(['/login']);
+  }
+
+  getUser(): User {
+    return {...this.user};
+  }
+
+  isAuth(): boolean {
+    return this.user != null;
+  }
+
+  private authSuccessfully() {
+    this.authChange.next(true);
+    this.router.navigate(['/training']);
+  }
+}
---
+++ src/app/navigation/header/header.component.ts
@@ -0,0 +1,37 @@
+import {Component, OnInit, EventEmitter, Output, OnDestroy} from '@angular/core';
+import {AuthService} from "../../auth/auth.service";
+import {Subscription} from "rxjs";
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit, OnDestroy {
+  @Output() sidenavToggle = new EventEmitter<void>();
+  isAuth = false;
+  authSubscription: Subscription;
+
+  constructor(private authService: AuthService) {
+  }
+
+  ngOnInit(): void {
+    this.authSubscription = this.authService.authChange.subscribe(authStatus => {
+      this.isAuth = authStatus;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.authSubscription.unsubscribe();
+  }
+
+  onToggleSidenav(): void {
+    this.sidenavToggle.emit();
+    console.log("sidenav toggle");
+  }
+
+  onLogout() {
+    this.authService.logout();
+  }
+
+}
